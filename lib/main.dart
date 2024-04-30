f2017a0fe892aac97e8c58a04ce15c4762c0bffd
Daniel Kim <dwkim0101@naver.com>
2024-04-30 15:25:10 +0900

    🎨 Bottom Bar Update
    Make BottomBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,105 +1,94 @@
+import 'package:awesome_bottom_bar/widgets/inspired/inspired.dart';
 import 'package:flutter/material.dart';
+import 'package:awesome_bottom_bar/awesome_bottom_bar.dart';
 
+List<TabItem> items = [
+  const TabItem(
+    icon: Icons.home,
+    // title: '홈',
+  ),
+  const TabItem(
+    icon: Icons.calendar_month,
+    // title: '월간 보기',
+  ),
+  const TabItem(
+    icon: Icons.star,
+    // title: '리뷰',
+  ),
+  const TabItem(
+    icon: Icons.account_circle,
+    // title: 'Cart',
+  ),
+  // TabItem(
+  //   icon: Icons.account_box,
+  //   title: 'profile',
+  // ),
+];
+List<Widget> viewWidgets = [
+  Background(items: items),
+  OutSide(items: items),
+  Salomon(items: items),
+  Fancy(items: items),
+];
 void main() {
-  runApp(
-    const MaterialApp(
-      title: 'Returning Data',
-      home: HomeScreen(),
-    ),
-  );
+  runApp(const MyApp());
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const BottomBarWidget(),
+    );
+  }
+}
+
+class BottomBarWidget extends StatefulWidget {
+  const BottomBarWidget({
+    super.key,
+    // required this.title,
+  });
+
+  // final String title;
+
+  @override
+  _BottomBarWidgetState createState() => _BottomBarWidgetState();
+}
+
+class _BottomBarWidgetState extends State<BottomBarWidget> {
+  int visit = 0;
+  double height = 30;
+  Color colorSelect = const Color(0XFF0686F8);
+  Color color = const Color.fromARGB(255, 78, 136, 186);
+  Color color2 = const Color(0XFF96B1FD);
+  Color bgColor = const Color(0XFF1752FE);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Returning Data Demo'),
-      ),
-      body: const Center(
-        child: SelectionButton(),
-      ),
-    );
-  }
-}
-
-class SelectionButton extends StatefulWidget {
-  const SelectionButton({super.key});
-
-  @override
-  State<SelectionButton> createState() => _SelectionButtonState();
-}
-
-class _SelectionButtonState extends State<SelectionButton> {
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        _navigateAndDisplaySelection(context);
-      },
-      child: const Text('Pick an option, any option!'),
-    );
-  }
-
-  // A method that launches the SelectionScreen and awaits the result from
-  // Navigator.pop.
-  Future<void> _navigateAndDisplaySelection(BuildContext context) async {
-    // Navigator.push returns a Future that completes after calling
-    // Navigator.pop on the Selection Screen.
-    final result = await Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => const SelectionScreen()),
-    );
-
-    // When a BuildContext is used from a StatefulWidget, the mounted property
-    // must be checked after an asynchronous gap.
-    if (!context.mounted) return;
-
-    // After the Selection Screen returns a result, hide any previous snackbars
-    // and show the new result.
-    ScaffoldMessenger.of(context)
-      ..removeCurrentSnackBar()
-      ..showSnackBar(SnackBar(content: Text('$result')));
-  }
-}
-
-class SelectionScreen extends StatelessWidget {
-  const SelectionScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Pick an option'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8),
-              child: ElevatedButton(
-                onPressed: () {
-                  // Close the screen and return "Yep!" as the result.
-                  Navigator.pop(context, 'Yep!');
-                },
-                child: const Text('Yep!'),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8),
-              child: ElevatedButton(
-                onPressed: () {
-                  // Close the screen and return "Nope." as the result.
-                  Navigator.pop(context, 'Nope.');
-                },
-                child: const Text('Nope.'),
-              ),
-            )
-          ],
-        ),
+      // appBar: AppBar(
+      //   title: Text(widget.title),
+      // ),
+      body: viewWidgets[visit],
+      bottomNavigationBar: BottomBarInspiredInside(
+        items: items,
+        backgroundColor: Colors.white,
+        color: color2,
+        colorSelected: Colors.white,
+        indexSelected: visit,
+        onTap: (int index) => setState(() {
+          visit = index;
+          print(visit);
+        }),
+        chipStyle: const ChipStyle(convexBridge: true),
+        itemStyle: ItemStyle.circle,
+        animated: true,
       ),
     );
   }
